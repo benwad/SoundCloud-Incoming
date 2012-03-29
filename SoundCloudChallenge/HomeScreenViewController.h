0e8b21ecd5dd7c3da8e6f26346fbc8e27203ba98
Ben Wadsworth <wadben@gmail.com>
2012-03-29 14:14:16 +0100

    Added initial network stuff

---
+++ SoundCloudChallenge/HomeScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeScreenViewController.h
+//  SoundCloudChallenge
+//
+//  Created by Ben Wadsworth Fixed on 29/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeScreenViewController : UIViewController
+
+@end
---
+++ SoundCloudChallenge/ViewControllerBase.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerBase.h
+//  SoundCloudChallenge
+//
+//  Created by Ben Wadsworth Fixed on 29/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerBase : UIViewController
+
+@end
