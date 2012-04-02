e760d73fb7553e38e1467f561ecdd43fd2be1344
Ben Wadsworth <wadben@Bens-MacBook-Pro.local>
2012-04-02 19:29:37 +0100

    removed EGOImageLoading submodule

---
+++ SoundCloudChallenge/TopHalfEGOImageView.h
@@ -0,0 +1,13 @@
+//
+//  TopHalfEGOImageView.h
+//  SoundCloudChallenge
+//
+//  Created by Ben Wadsworth on 01/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "EGOImageView.h"
+
+@interface TopHalfEGOImageView : EGOImageView
+
+@end
---
+++ SoundCloudChallenge/TopHalfEGOImageView.m
@@ -0,0 +1,31 @@
+//
+//  TopHalfEGOImageView.m
+//  SoundCloudChallenge
+//
+//  Created by Ben Wadsworth on 01/04/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TopHalfEGOImageView.h"
+
+@implementation TopHalfEGOImageView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
